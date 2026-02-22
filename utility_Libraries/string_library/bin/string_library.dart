c890b0834a4f7a48a36c2a30979c9bf0be405708
engIbrahemQruea <mmany6186@gmail.com>
2026-02-23 02:32:40 +0300

    chore: add utility_Libraries structure

---
+++ utility_Libraries/string_library/bin/string_library.dart
@@ -0,0 +1,5 @@
+import 'package:string_library/string_library.dart' as string_library;
+
+void main(List<String> arguments) {
+  print('Hello world: ${string_library.calculate()}!');
+}
---
+++ utility_Libraries/string_library/test/string_library_test.dart
@@ -0,0 +1,8 @@
+import 'package:string_library/string_library.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
