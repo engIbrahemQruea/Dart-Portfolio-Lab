27bb6ccef0df6265567670e87680d242aae483b0
engIbrahemQruea <mmany6186@gmail.com>
2026-03-28 14:52:49 +0300

    add: create constance.dart to define file paths and encryption key for bank system

---
+++ bank_system/lib/core/utils/constance.dart
@@ -0,0 +1,9 @@
+const String clientFileName = 'bank_system/data/Clients.txt';
+const String userFileName = 'bank_system/data/Users.txt';
+const String logFileName = 'bank_system/data/Log_register.txt';
+const String transactionFileName = 'bank_system/data/Transfer_log.txt'; 
+const String encryptionKey = "IbrahimQureaBankKey#2026";
+const String currencyFileName = 'bank_system/data/Currencies.txt';
+
+
+
